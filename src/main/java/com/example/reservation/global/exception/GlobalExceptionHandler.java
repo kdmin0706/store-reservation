1fdb5c84fb7cacba5d78acfd69fc084fe3500afc
kdmin0706 <gxxk76@gmail.com>
2023-11-17 15:12:48 +0900

    commit

--- src/main/java/com/example/reservation/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/reservation/global/exception/GlobalExceptionHandler.java
@@ -7,10 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
-
 import static com.example.reservation.global.type.ErrorCode.INTERNAL_SERVER_ERROR;
-import static com.example.reservation.global.type.ErrorCode.INVALID_ACCESS;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,14 +17,6 @@ public class GlobalExceptionHandler {
                 e.getErrorCode(), e.getErrorMessage());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<?> accessDeniedExceptionHandler(AccessDeniedException e) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                INVALID_ACCESS, INVALID_ACCESS.getDescription());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(Exception.class)
