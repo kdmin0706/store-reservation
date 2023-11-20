6432f265a699ba3a34a9e2e1e2cf041dc32dd663
kdmin0706 <gxxk76@gmail.com>
2023-11-21 01:53:45 +0900

    manager commit

---
+++ src/main/java/com/example/storeReservation/manager/dto/RegisterManager.java
@@ -0,0 +1,25 @@
+package com.example.storeReservation.manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+public class RegisterManager {
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    public RegisterManager from(ManagerDto managerDto) {
+        return RegisterManager.builder()
+                .username(managerDto.getUsername())
+                .email(managerDto.getEmail())
+                .password(managerDto.getPassword())
+                .phoneNumber(managerDto.getPhoneNumber())
+                .build();
+    }
+}
