bce1c4d7280bf510a96dd0abb13d7fd39d135eb0
kdmin0706 <gxxk76@gmail.com>
2023-11-28 00:25:52 +0900

    Auth commit

--- src/main/java/com/example/storeReservation/auth/security/AuthenticationFilter.java
+++ src/main/java/com/example/storeReservation/auth/security/AuthenticationFilter.java
@@ -1,12 +1,8 @@
 package com.example.storeReservation.auth.security;
 
-import com.example.storeReservation.global.exception.CustomException;
-import com.example.storeReservation.global.type.ErrorCode;
-import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
